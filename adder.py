b6d64a022047f30e034001b8bcc40268721ac3c2
Josh Cox <josh@webhosting.coop>
2019-10-08 21:03:08 -0500

    bonus

--- adder.py
+++ adder.py
@@ -3,7 +3,7 @@ def my_adder(list1, list2):
     if len(list1) == len(list2):
         pass
     else:
-        raise NameError('Unequal list length')
+        raise ValueError("Given matrices are not the same size.")
     added = list()
     for x in range (len(list1)):
         added.append( list1[x] + list2[x] )
@@ -30,6 +30,16 @@ def matrix_adder(listA, listB):
         matrixadded.append( my_adder(listA[x], listB[x]))
     return matrixadded
 
+def matrix_adder_overloaded(*args):
+    resultmatrix = list()
+    for x in range (len(args)):
+        if x == 0:
+          resultmatrix = args[x]
+        else:
+          resultmatrix = matrix_adder(resultmatrix, args[x])
+        #print (resultmatrix)
+    return resultmatrix
+
 def matrix_printer(listx):
     my_printer(listx)
 
@@ -45,4 +55,11 @@ matrix2 = [[2, -1], [0, -1]]
 matrix3 = matrix_adder( matrix1, matrix2 )
 #matrix_printer(matrix3)
 
+print("Here is the desired answer")
 print (matrix_adder( matrix1, matrix2 ))
+
+print("Here is the answer for bonus 1")
+print (matrix_adder_overloaded([[1, 9], [7, 3]], [[5, -4], [3, 3]], [[2, 3], [-3, 1]]))
+
+print("And finally the desired error state for bonus 2\n\n")
+print (matrix_adder_overloaded([[1, 9], [7, 3]], [[1, 2], [3]]))
